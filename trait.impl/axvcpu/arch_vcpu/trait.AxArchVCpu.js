e5c77bab1015a082a72bc6a8aeb1e80388f619c7
hky1999 <976929993@qq.com>
2024-10-23 08:26:24 +0000

    Deploying to gh-pages from @ arceos-hypervisor/arm_vcpu@13405844b2c5828971a5bcab2151ec105af9af0e 🚀

---
+++ trait.impl/axvcpu/arch_vcpu/trait.AxArchVCpu.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["arm_vcpu",[["impl AxArchVCpu for <a class=\"struct\" href=\"arm_vcpu/struct.Aarch64VCpu.html\" title=\"struct arm_vcpu::Aarch64VCpu\">Aarch64VCpu</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[155]}
