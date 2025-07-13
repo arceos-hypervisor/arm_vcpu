ce67fceca21195f226ee5f70211204413b6bcff3
aarkegz <aarkegz@gmail.com>
2025-07-13 23:03:24 +0800

    fix a missing doc

--- src/sysreg/mod.rs
+++ src/sysreg/mod.rs
@@ -13,6 +13,7 @@ pub use cntpct_el0::SysCntpctEl0;
 mod cntp_tval_el0;
 pub use cntp_tval_el0::SysCntpTvalEl0;
 
+/// Create a collection of system register devices.
 pub fn get_sysreg_device() -> Vec<Arc<dyn BaseSysRegDeviceOps>> {
     vec![
         Arc::new(SysCntpCtlEl0::new()),
