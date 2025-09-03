02f1254342f4a69aff862bf3536eec8d7eb9a94e
Josen-B <65878371+Josen-B@users.noreply.github.com>
2025-09-03 07:11:32 +0000

    Deploying to gh-pages from @ arceos-hypervisor/arm_vcpu@5169a9cee69754485da40467a5da35038b6068e0 🚀

---
+++ trait.impl/core/default/trait.Default.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["arm_vcpu",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"arm_vcpu/struct.Aarch64VCpuCreateConfig.html\" title=\"struct arm_vcpu::Aarch64VCpuCreateConfig\">Aarch64VCpuCreateConfig</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"arm_vcpu/struct.Aarch64VCpuSetupConfig.html\" title=\"struct arm_vcpu::Aarch64VCpuSetupConfig\">Aarch64VCpuSetupConfig</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[633]}
