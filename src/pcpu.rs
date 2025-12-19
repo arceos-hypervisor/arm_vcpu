012bc4cd730512f12d9cb2f63380c3b6b4fa728d
周睿 <zrufo747@outlook.com>
2025-12-19 15:41:57 +0800

    fix: 修正 hardware_enable 方法中的类型转换以确保安全性

--- src/pcpu.rs
+++ src/pcpu.rs
@@ -26,7 +26,7 @@ impl Aarch64PerCpu {
     pub fn hardware_enable(&mut self) {
         // Set current `VBAR_EL2` to `exception_vector_base_vcpu`
         // defined in this crate.
-        VBAR_EL2.set(exception_vector_base_vcpu as usize as _);
+        VBAR_EL2.set(exception_vector_base_vcpu as *const () as usize as _);
 
         HCR_EL2.modify(
             HCR_EL2::VM::Enable + HCR_EL2::RW::EL1IsAarch64 + HCR_EL2::TSC::EnableTrapEl1SmcToEl2,
